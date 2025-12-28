c4f51c27fdee7ddabfb7ecbfdad104a5cc5b0bd7
Ajay Katrapati <ajaykatrapati@Ajays-MacBook-Air.local>
2025-12-28 16:03:54 -0500

    first project

---
+++ infra/main.tf
@@ -0,0 +1,25 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_service_plan" "app_service_plan" {
+  name                = "${var.app_service_name}-plan"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "web_app" {
+  name                = var.app_service_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.app_service_plan.location
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "8.0"
+    }
+    always_on = false
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "kml_rg_main-afde299b3e854821"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "app_service_name" {
+  default = "webapp-simplenetapp-dev-001"
+}
